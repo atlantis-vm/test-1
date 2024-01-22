ac2f99a208a535240766b55e3f04be92f6ad14bf
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-22 18:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,3 @@ resource "google_compute_instance" "default" {
     }
   }
 }
-
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucketttt"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
