17c36c7d2e70d084af95a785ce0a29906f063d7e
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-23 10:51:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,35 +2,3 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
-resource "google_compute_instance" "default" {
-  name         = var.instance_name
-  machine_type = "e2-micro"
-  
-  zone         = var.zone
-  tags = ["dev", "engineering"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        env = "dev"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
-
-
-resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucketttt"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-}
