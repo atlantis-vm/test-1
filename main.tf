4ae56358e9b837ab050f02ff66249bb467b9b21b
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-23 11:11:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
+resource "google_storage_bucket" "auto-expire" {
+  name          = "no-public-access-bucketttt"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
